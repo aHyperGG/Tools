28b619aae85a9aca821f92d09f1f73ad4b45f201
aHyperGG <106444541+aHyperGG@users.noreply.github.com>
2022-11-12 18:23:07 +0100

    Add files via upload

--- Toolt.py
+++ Toolt.py
@@ -1,25 +1,3 @@
-# My tools!
-import os
-import time
-import hoting
-
-#Ping Sweeping!
-def pingsweep():
-	is_prefix = input("Is the ip prefix 192.168?: ")
-    while is_prefix.lower != null:
-        if is_prefix.lower == "yes":
-                prefix = is_prefix
-                break
-        elif is_prefix.lower == "no":
-            is_prefix = input("Please Enter the prefix of the ip address: ")
-            is_prefix = prefix
-            break
-        else:
-            print("Please Enter a valid answer ex: yes or no")
-    ping(prefix)
-    ping(is_prefix)
-# To be continued
-
 #BruteForcing!
 
 def BruteForcer():
@@ -32,11 +10,13 @@ def BruteForcer():
     list = (file.read())
     ready = input("Ready?")
     if ready.lower == "yes":
-        print("LETS GOOO BEACUSE IM "hotness()"!!")
+        print("Hello")
+        #print("LETS GOOO BEACUSE IM ", hotness()"!!")
     elif ready.lower == "no":
-        ready = input("Tell me when your ready cause "hotness()" ex:just write out ready!")
+        print("ok")
+        #ready = input("Tell me when your ready cause "hotness()" ex:just write out ready!")
     else:
-        while ready.lower != "yes" or "no"
+        while ready.lower != "yes" or "no":
             print("Please enter a valid answer! eg: yes or no!!")
     print("You have 10 seconds to open the window with the auth to crack!!!!!! LETS GOOOOOOOOOOOO!!!!!!!!!!!")
     print("Press any letter to skip 1 second")
---
+++ test.py
@@ -0,0 +1,3 @@
+import Toolt
+
+BruteForcer()
